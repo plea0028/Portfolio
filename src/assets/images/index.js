db9d1539daba270ba590909826ee20b60eb853f7
James Pleau <plea0028@algonquinlive.com>
2026-09-06 17:23:18 -0400

    feat: fixed typo in navScreenLeft

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,6 @@ export { default as algonquinGo } from './algonquinGo.png';
 export { default as konnichiwa } from './konnichiwa.png';
 export { default as tunementor } from './tunementor.png';
 export { default as AllInOneAd } from './AllInOneAd.png';
-export { default as AlgonquinAd1 } from './NavScreenLeft.png';
+export { default as AlgonquinAd1 } from './navScreenLeft.png';
 export { default as AlgonquinAd2 } from './reportScreen.png';
 export { default as AlgonquinAd3 } from './eventsScreen.png';
