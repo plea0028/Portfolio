8d6293e1fd53f662773f1a5fbde3b8c7b9d688a5
James Pleau <plea0028@algonquinlive.com>
2026-09-06 17:55:24 -0400

    feat: renamed and restructured the app

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
-export { default as algonquinGo } from './algonquinGo.png';
-export { default as konnichiwa } from './konnichiwa.png';
-export { default as tunementor } from './tunementor.png';
-export { default as AllInOneAd } from './AllInOneAd.png';
-export { default as AlgonquinAd1 } from './navScreenLeft.png';
-export { default as AlgonquinAd2 } from './reportScreen.png';
-export { default as AlgonquinAd3 } from './eventsScreen.png';
+export { default as algonquinGo } from './algonquin/algonquin-go-hero.png';
+export { default as konnichiwa } from './konnichiwa/konnichiwa-hero.png';
+export { default as tunementor } from './tunementor/tunementor-hero.png';
+export { default as AllInOneAd } from './algonquin/algonquin-featured.png';
+export { default as AlgonquinAd1 } from './algonquin/algonquin-scene-01.png';
+export { default as AlgonquinAd2 } from './algonquin/algonquin-scene-02.png';
+export { default as AlgonquinAd3 } from './algonquin/algonquin-scene-03.png';
